5f49f4c58d5c5d1f32fcd66de6ab35e296bad4a3
$ <omniiknight@hotmail.com>
2018-10-08 11:30:37 +0200

    adding projects page

--- app.js
+++ app.js
@@ -53,6 +53,13 @@ app.get("/about", (req, res) => {
     });
 });
 
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Projects page",
+        welcomeMsg: "These are my projects...",
+    });
+});
+
 app.get("/bad", (req, res) => {
     res.send({
         error: "this is error msg",
